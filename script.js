3e48d9950bba45915c21254bb8a945646b38c44d
michellekittleson <michelle.kittleson@gmail.com>
2024-12-09 14:57:45 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// Understanding Data Types in JavaScript
+
+// Task 2: Assign sample values
+let studentName = "Alice";
+let studentAge = 15;
+let studentGrade = 10;
+let isPresent = true;
+
+// Task 3: Display student information using console.log() statements
+console.log("Student Information:")
+console.log("- Name:", studentName);
+console.log("- Age:", studentAge);
+console.log("- Grade:", studentGrade);
+console.log("- Present?", isPresent ? "Yes" : "No");
+
+
+// Exploring JavaScript Operators
+
+let x = 4;
+let y = 7;
+
+// Task 3: Perform arithmetic operations
+console.log(`Sum: ${x} + ${y} = ${x + y}`);
+console.log(`Difference: ${x} - ${y} = ${x - y}`);
+console.log(`Product: ${x} * ${y} = ${x * y}`);
+console.log(`Quotient: ${x} / ${y} = ${x / y}`);
+
+// Task 4: Explore assignment operators
+x = 10;
+y = 20;
+console.log("Updated x:", x);
+console.log("Updated y:", y);
+
+// Task 5: Use comparison operators 
+console.log("Is x equal to y?", x === y ? "Yes" : "No");
+console.log("Is x greater than y?", x > y ? "Yes" : "No");
+console.log("Is x not equal to y?", x!==y ? "Yes" : "No");
+
+// Task 6: Apply logical operators
+let isPositive = x > 0 && y > 0;
+let isEven = x % 2 == 0 || y % 2 == 0;
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
